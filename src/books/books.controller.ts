51d1bac9ed8dd4ffeeac5121ff8c6e94798d1469
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-01-08 18:34:49 -0300

    fix:books controller routes

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -8,20 +8,21 @@ import {
   Delete,
   Query,
   ValidationPipe,
-  ParseUUIDPipe,
   UsePipes,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { GetBookDto } from './dto/get-book.dto';
+import { DeleteBookDto } from './dto/delete-book.dto';
 
 @Controller('books')
+@UsePipes(ValidationPipe)
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Post()
-  create(@Body(new ValidationPipe()) createBookDto: CreateBookDto) {
+  create(@Body() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
   }
 
@@ -31,19 +32,17 @@ export class BooksController {
   }
 
   @Get(':id')
-  @UsePipes(ValidationPipe)
   findOne(@Param() { id }: GetBookDto) {
     return this.booksService.findOne(id);
   }
 
   @Patch(':id')
-  @UsePipes(ValidationPipe)
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+  update(@Param() { id }: UpdateBookDto, @Body() updateBookDto: UpdateBookDto) {
     return this.booksService.update(id, updateBookDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param() { id }: DeleteBookDto) {
     return this.booksService.remove(id);
   }
 }
