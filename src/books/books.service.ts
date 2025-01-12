5cdf5472c3ec8413beeb101f25d7ebef08cab2c0
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-01-12 16:38:36 -0300

    feat: insert book with author

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -4,16 +4,24 @@ import { UpdateBookDto } from './dto/update-book.dto';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { desc, eq } from 'drizzle-orm';
 import * as schema from '../drizzle/schema';
+import { AuthorsService } from 'src/authors/authors.service';
 
 @Injectable()
 export class BooksService {
   constructor(
     @Inject('DrizzleProvider')
     private readonly db: NodePgDatabase<typeof schema>,
+    private readonly authorService: AuthorsService,
   ) {}
 
-  create(createBookDto: CreateBookDto) {
-    return this.db.insert(schema.books).values([createBookDto]).returning()
+  async create({ author, ...createBookDto }: CreateBookDto) {
+    if (author) {
+      const createAuthor = this.authorService.create(author);
+      const [{ id }] = await createAuthor;
+      createBookDto.authorId = id;
+      return this.db.insert(schema.books).values([createBookDto]).returning();
+    }
+    return this.db.insert(schema.books).values([createBookDto]).returning();
   }
 
   findAll(limit: string, skip: string) {
@@ -22,15 +30,19 @@ export class BooksService {
       .from(schema.books)
       .limit(+limit || 50)
       .offset(+skip || 0)
-      .orderBy(desc(schema.books.rating))
+      .orderBy(desc(schema.books.rating));
   }
 
   findOne(id: string) {
-    return this.db.select().from(schema.books).where(eq(schema.books.id, id))
+    return this.db.select().from(schema.books).where(eq(schema.books.id, id));
   }
 
   update(id: string, updateBookDto: UpdateBookDto) {
-    return this.db.update(schema.books).set(updateBookDto).where(eq(schema.books.id, id)).returning()
+    return this.db
+      .update(schema.books)
+      .set(updateBookDto)
+      .where(eq(schema.books.id, id))
+      .returning();
   }
 
   remove(id: string) {
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,4 +1,6 @@
-import { IsNotEmpty, IsNumber, IsNumberString, IsString, Length } from "class-validator"
+import { Type } from "class-transformer"
+import { IsNotEmpty, IsNumber, IsNumberString, IsString, Length, ValidateIf, ValidateNested } from "class-validator"
+import { CreateAuthorDto } from "src/authors/dto/create-author.dto"
 
 export class CreateBookDto {
       @IsString()
@@ -11,9 +13,15 @@ export class CreateBookDto {
       @IsNumber()
       @IsNotEmpty()
       pricePerDay: number
+      @ValidateIf(o => !o.author)
       @IsNotEmpty()
       authorId: string
       @IsNumberString()
       @IsNotEmpty()
       rating: '1' | '2' | '3' | '4' | '5';
+
+      @ValidateIf(o => o.author)
+      @ValidateNested()
+      @Type(() => CreateAuthorDto)
+      author: CreateAuthorDto
 }
