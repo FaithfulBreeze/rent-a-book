52cfd19e1cfbd3e0feaebfd0b530bd4ad591342d
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-01-13 22:42:57 -0300

    refactor: schema refactor

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { boolean, pgEnum, pgTable, real, uuid, varchar } from 'drizzle-orm/pg-core';
+import { boolean, date, integer, pgEnum, pgTable, real, uuid, varchar } from 'drizzle-orm/pg-core';
 
 export const rating = pgEnum('rating', ['1', '2', '3', '4', '5']);
 
@@ -10,7 +10,16 @@ export const libraries = pgTable("libraries", {
 
 export const stock = pgTable("stock", {
   id: uuid().primaryKey().defaultRandom(),
-  books: uuid().references(() => books.id)
+  bookId: uuid('book_id').references(() => books.id).unique().notNull(),
+  quantity: integer().default(0),
+  available: integer().default(0),
+  borrowed: integer().default(0),
+})
+
+export const stockHistory = pgTable("stock_history", {
+  bookId: uuid('book_id').references(() => stock.bookId).notNull(),
+  userId: uuid('user_id').references(() => users.id).notNull(),
+  borrowEndDate: date('borrow_end_date').notNull(),
 })
 
 export const books = pgTable('books', {
@@ -18,7 +27,7 @@ export const books = pgTable('books', {
   title: varchar('title', { length: 30 }).notNull(),
   description: varchar().notNull(),
   pricePerDay: real('price_per_day').notNull(),
-  authorId: uuid('authorId').references(() => authors.id).notNull(),
+  authorId: uuid('author_id').references(() => authors.id).notNull(),
   rating: rating().notNull(),
 });
 
@@ -28,13 +37,12 @@ export const authors = pgTable('authors', {
   rating: rating().notNull(),
 });
 
-
 export const users = pgTable('users', {
   id: uuid().primaryKey().defaultRandom(),
   name: varchar('name', { length: 30 }).notNull(),
   email: varchar().notNull().unique(),
   password: varchar().notNull(),
-  libraryId: uuid('libraryId').references(() => libraries.id),
+  libraryId: uuid('library_id').references(() => libraries.id),
   hasLibrary: boolean('has_library').notNull().default(false),
   accessToken: varchar('access_token'),
 })
