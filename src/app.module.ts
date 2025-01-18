20dc474804e117b038412fc301f47a4176689daf
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-01-18 17:43:22 -0300

    feat: encryption module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { DrizzleModule } from './drizzle/drizzle.module';
 import { BooksModule } from './books/books.module';
 import { RedisModule } from './redis/redis.module';
 import { UsersModule } from './users/users.module';
+import { EncryptionModule } from './encryption/encryption.module';
 
 @Module({
-  imports: [DrizzleModule, BooksModule, RedisModule, UsersModule],
+  imports: [
+    DrizzleModule,
+    BooksModule,
+    RedisModule,
+    UsersModule,
+    EncryptionModule,
+  ],
   controllers: [],
   providers: [],
 })
