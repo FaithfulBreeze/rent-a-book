23a7d7d4933f07adbaada10f94b1f7750db6db32
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-01-19 16:27:41 -0300

    feat: mailer module and email validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { EncryptionModule } from './encryption/encryption.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from './jwt/jwt.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { JwtModule } from './jwt/jwt.module';
     EncryptionModule,
     AuthModule,
     JwtModule,
+    MailerModule,
   ],
   controllers: [],
   providers: [],
