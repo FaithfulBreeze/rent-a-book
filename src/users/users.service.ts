025ce46d9f557b12065991e51423da9dceb156cf
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-01-19 17:08:49 -0300

    refactor: user creation now have validation on email and must have confirmPassword field

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { randomBytes } from 'crypto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from 'src/drizzle/schema';
 import { eq, getTableColumns } from 'drizzle-orm';
 import { EncryptionService } from 'src/encryption/encryption.service';
+import { MailerService } from 'src/mailer/mailer.service';
+import { RedisService } from 'src/redis/redis.service';
+import { ContextCreator } from '@nestjs/core/helpers/context-creator';
 
 @Injectable()
 export class UsersService {
@@ -12,8 +16,21 @@ export class UsersService {
     @Inject('DrizzleProvider')
     private readonly db: NodePgDatabase<typeof schema>,
     private readonly encryptionService: EncryptionService,
+    private readonly mailerService: MailerService,
+    private readonly redisService: RedisService,
   ) {}
-  async create(createUserDto: CreateUserDto) {
+
+  async create(createUserDto: CreateUserDto, validationCode?: string) {
+    if (createUserDto.confirmPassword != createUserDto.password)
+      throw new BadRequestException(undefined, 'Passwords does not match');
+    delete createUserDto.confirmPassword;
+
+    const result = await this.validateEmail(
+      createUserDto.email,
+      validationCode,
+    );
+    if (result) return result;
+
     createUserDto.password = await this.encryptionService.encrypt(
       createUserDto.password,
     );
@@ -39,5 +56,38 @@ export class UsersService {
 
   remove(id: string) {
     return this.db.delete(schema.users).where(eq(schema.users.id, id));
+  }
+
+  async sendEmailValidationMessage(email: string) {
+    const code = randomBytes(3).toString('hex');
+    await this.redisService.storeValidationCode(email, code);
+    this.mailerService.sendValidationCode(email, code);
+  }
+
+  async checkValidationCode(email: string) {
+    return await this.redisService.getValidationCode(email);
+  }
+
+  async validateEmail(email: string, validationCode?: string) {
+    if (
+      (
+        await this.db
+          .select()
+          .from(schema.users)
+          .where(eq(schema.users.email, email))
+      ).length > 0
+    )
+      throw new BadRequestException('Email already in use');
+
+    if (!validationCode) {
+      await this.sendEmailValidationMessage(email);
+      return {
+        message:
+          'An email validation code was sent to your email, send it back as a query value on field (validation_code), the code will expire in 1 minute',
+      };
+    }
+
+    if ((await this.checkValidationCode(email)) !== validationCode)
+      throw new BadRequestException(undefined, 'Code not valid or expired');
   }
 }
