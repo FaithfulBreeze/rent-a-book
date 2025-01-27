a0f62ddab2bd6d2cb3073107d3d9c6ef96f3876e
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-01-26 23:49:48 -0300

    test:users tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,17 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UsersModule } from './users.module';
 import { DrizzleModule } from 'src/drizzle/drizzle.module';
+import { EncryptionModule } from 'src/encryption/encryption.module';
+import { JwtModule } from 'src/jwt/jwt.module';
+import { MailerModule } from 'src/mailer/mailer.module';
+import { RedisModule } from 'src/redis/redis.module';
+import { getCreateUserDto, getUpdateUserDto } from 'src/__mock__';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    create: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
+    jest.resetAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule, DrizzleModule],
+      imports: [
+        DrizzleModule,
+        RedisModule,
+        EncryptionModule,
+        JwtModule,
+        MailerModule,
+      ],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [{ provide: UsersService, useValue: mockUsersService }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -19,5 +37,30 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call create with createUserDto', async () => {
+    const createUserDto = getCreateUserDto();
+    await controller.create(createUserDto, undefined);
+    expect(mockUsersService.create).toHaveBeenCalledWith(
+      createUserDto,
+      undefined,
+    );
+  });
+
+  it('should call findOne with id: "4"', async () => {
+    await controller.findOne('4');
+    expect(mockUsersService.findOne).toHaveBeenCalledWith('4');
+  });
+
+  it('should call update with id: "3" and updateUserDto', async () => {
+    const updateUserDto = getUpdateUserDto();
+    await controller.update('3', updateUserDto);
+    expect(mockUsersService.update).toHaveBeenCalledWith('3', updateUserDto);
+  });
+
+  it('should call remove with id: "2"', async () => {
+    await controller.remove('2');
+    expect(mockUsersService.remove).toHaveBeenCalledWith('2');
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import { UsersModule } from './users.module';
 import { DrizzleModule } from 'src/drizzle/drizzle.module';
+import { EncryptionModule } from 'src/encryption/encryption.module';
+import { JwtModule } from 'src/jwt/jwt.module';
+import { MailerModule } from 'src/mailer/mailer.module';
+import { RedisModule } from 'src/redis/redis.module';
+import {
+  getCreateUserDto,
+  getMockDrizzleProvider,
+  getMockMailerService,
+  getMockRedisService,
+} from 'src/__mock__';
+import { RedisService } from 'src/redis/redis.service';
+import { MailerService } from 'src/mailer/mailer.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockDrizzleProvider = getMockDrizzleProvider({ fakeInsert: true });
+  const mockRedisService = getMockRedisService();
+  const mockMailerService = getMockMailerService();
+
   beforeEach(async () => {
+    jest.clearAllMocks();
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule, DrizzleModule],
-      providers: [UsersService],
+      imports: [
+        DrizzleModule,
+        RedisModule,
+        EncryptionModule,
+        JwtModule,
+        MailerModule,
+      ],
+      providers: [
+        UsersService,
+        { provide: 'DrizzleProvider', useValue: mockDrizzleProvider },
+        { provide: RedisService, useValue: mockRedisService },
+        { provide: MailerService, useValue: mockMailerService },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -18,4 +45,75 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should send email validation code', async () => {
+    const createUserDto = getCreateUserDto();
+    const { message } = (await service.create(createUserDto)) as {
+      message: string;
+    };
+    expect(message).toBe(
+      'An email validation code was sent to your email, send it back as a query value on field (validation_code), the code will expire in 1 minute',
+    );
+  });
+
+  it('should throw if validation code is invalid or expired', async () => {
+    const createUserDto = getCreateUserDto();
+    await service.create(createUserDto);
+    const validationCode = 'invalid_validation_code';
+    try {
+      await service.create(getCreateUserDto(), validationCode);
+    } catch (error) {
+      expect(error.message).toBe('Code not valid or expired');
+    }
+  });
+
+  it('should create a user', async () => {
+    const createUserDto = getCreateUserDto();
+    await service.create(createUserDto);
+    const validationCode = mockRedisService.getValidationCode(
+      createUserDto.email,
+    );
+    expect(
+      await service.create(getCreateUserDto(), validationCode),
+    ).toHaveProperty('id');
+  });
+
+  it('should throw error if email already taken', async () => {
+    const createUserDto = getCreateUserDto();
+    createUserDto.email = 'josephdoe@gmail.com';
+    try {
+      await service.create(createUserDto);
+    } catch (error) {
+      expect(error.message).toBe('Email already in use');
+    }
+  });
+
+  it('should throw error if password != confirmPassword', async () => {
+    const createUserDto = getCreateUserDto();
+    createUserDto.password = 'differentPassword';
+    try {
+      await service.create(createUserDto);
+    } catch (error) {
+      expect(error.message).toBe('Passwords does not match');
+    }
+  });
+
+  it('should find user with id: "1"', async () => {
+    expect(await service.findOne('1')).toHaveProperty('name', 'Joseph Doe');
+  });
+
+  it('should update a user', async () => {
+    const userToBeUpdated = mockDrizzleProvider.users[0];
+    expect(
+      await service.update(userToBeUpdated.id, {
+        email: 'updatedemail@gmail.com',
+      }),
+    ).toHaveProperty('email', 'updatedemail@gmail.com');
+  });
+
+  it('should remove a user', async () => {
+    const userToBeDeleted = mockDrizzleProvider.users[0]
+    await service.remove(userToBeDeleted.id)
+    expect(mockDrizzleProvider.users.length).toBe(0)
+  })
 });
