6ac28c6f0f043792d730d7dfefdefc35973031b6
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-02-23 12:53:09 -0300

    feature: add mailer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { RedisModule } from 'redis/redis.module';
+import { MailerModule } from 'mailer/mailer.module';
 
 @Module({
-  imports: [DrizzleModule, RedisModule],
+  imports: [DrizzleModule, RedisModule, MailerModule],
   controllers: [],
   providers: [],
 })
