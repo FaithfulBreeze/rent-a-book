3faca67479d5a9a700b25090fa3fcfc0c1b359b5
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-02-23 12:50:31 -0300

    feature: add redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DrizzleModule } from './drizzle/drizzle.module';
+import { RedisModule } from 'redis/redis.module';
 
 @Module({
-  imports: [DrizzleModule],
+  imports: [DrizzleModule, RedisModule],
   controllers: [],
   providers: [],
 })
