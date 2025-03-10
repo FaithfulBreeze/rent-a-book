47992017e5c5cd89d87a22b4c37f1e5b450c51a2
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-03-10 14:25:37 -0300

    refactor: auth repository to implement interfaces

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -2,16 +2,17 @@ import { Inject, Injectable } from '@nestjs/common';
 import { eq } from 'drizzle-orm';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from 'drizzle/schema';
-import { LoginDto } from './dto/login.dto';
+import { RepositoryFindOne } from 'common/interfaces/repository-find-one/repository-find-one.interface';
+import { User } from 'users/entities/user.entity';
 
 @Injectable()
-export class AuthRepository {
+export class AuthRepository implements RepositoryFindOne<User> {
   constructor(
     @Inject('DrizzleService')
     private readonly db: NodePgDatabase<typeof schema>,
   ) {}
 
-  getUser(loginDto: LoginDto) {
-    return this.db.select().from(schema.users).where(eq(schema.users.email, loginDto.email));
+  async findOne(value: string) {
+    return (await this.db.select().from(schema.users).where(eq(schema.users.email, value)))[0];
   }
 }
