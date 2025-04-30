7dc622bb45a14ce64d1778bb20725bae9e231f4e
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-04-29 22:37:23 -0300

    fix interceptor not saving token

--- src/common/interceptors/append-token/append-token.interceptor.ts
+++ src/common/interceptors/append-token/append-token.interceptor.ts
@@ -1,15 +1,21 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { JwtService } from 'jwt/jwt.service';
 import { Observable, tap } from 'rxjs';
+import { UsersRepository } from 'users/users.repository';
 
 @Injectable()
 export class AppendTokenInterceptor implements NestInterceptor {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersRepository: UsersRepository,
+  ) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       tap(({ id }) => {
         const res = context.switchToHttp().getResponse();
-        res.cookie('jwt', this.jwtService.generateToken(id), {
+        const token = this.jwtService.generateToken(id);
+        this.usersRepository.update(id, { acessToken: token });
+        res.cookie('jwt', token, {
           httpOnly: true,
           maxAge: 1000 * 60 * 60,
         });
