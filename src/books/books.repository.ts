bae1b9cde8b34756262d16900030ba48d5d56a75
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-05-18 01:18:12 -0300

    feat: add include-author query param

--- src/books/books.repository.ts
+++ src/books/books.repository.ts
@@ -1,33 +1,36 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { RepositoryCreate } from 'common/interfaces/repository-create/repository-create.interface';
-
-import { RepositoryFindOne } from 'common/interfaces/repository-find-one/repository-find-one.interface';
-import { RepositoryFindAll } from 'common/interfaces/repository-find-all/repository-find-all.interface';
-import { RepositoryRemove } from 'common/interfaces/repository-remove/repository-remove.interface';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from 'drizzle/schema';
 import { and, eq, getTableColumns, ilike, SQL } from 'drizzle-orm';
-import { Book } from './entities/book.entity';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
-export class BooksRepository
-  implements RepositoryCreate<Book>, RepositoryFindOne<Book>, RepositoryFindAll<Book>, RepositoryRemove
-{
+export class BooksRepository {
   constructor(@Inject('DrizzleService') private readonly db: NodePgDatabase<typeof schema>) {}
 
   async create(createBookDto: CreateBookDto) {
     return (await this.db.insert(schema.books).values(createBookDto).returning())[0];
   }
 
-  async findAll(limit?: number, offset?: number, author?: string, name?: string) {
+  async findAll(limit?: number, offset?: number, author?: string, name?: string, includeAuthor?: boolean) {
     const filters: SQL[] = [];
 
     if (name) filters.push(ilike(schema.books.name, `%${name}%`));
     if (author) filters.push(eq(schema.books.authorId, author));
     //eslint-disable-next-line  @typescript-eslint/no-unused-vars
-    const { content, ...fields } = getTableColumns(schema.books);
+    const { content, authorId, ...fields } = getTableColumns(schema.books);
+
+    if (includeAuthor) {
+      const authorColumns = getTableColumns(schema.authors);
+      return await this.db
+        .select({ ...fields, author: { ...authorColumns } })
+        .from(schema.books)
+        .where(and(...filters))
+        .limit(limit || 10)
+        .offset(offset || 0)
+        .fullJoin(schema.authors, eq(schema.books.authorId, schema.authors.id));
+    }
 
     return await this.db
       .select(fields)
